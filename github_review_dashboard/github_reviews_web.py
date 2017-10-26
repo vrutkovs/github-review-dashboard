44dcd609710f8f0867a458b9affba9347a315d42
Vadim Rutkovsky <vrutkovs@redhat.com>
2017-10-26 11:18:19 +0200

    Use SSE instead of WebSockets, as there is just one-way communication here

--- github_review_dashboard/github_reviews_web.py
+++ github_review_dashboard/github_reviews_web.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
-from aiohttp import web, WSMsgType, WSCloseCode
+from aiohttp import web
+from aiohttp_sse import sse_response
 import aiohttp_jinja2
 import jinja2
 import json
@@ -21,30 +22,21 @@ async def user_report(request):
 
 
 async def ws(request):
-    user = request.match_info['user']
-    ws = web.WebSocketResponse()
-    request.app['websockets'].append(ws)
-    await ws.prepare(request)
-    (client, prs) = github_reviews.prepare_report(user)
-
-    async for msg in ws:
-        if msg.tp == WSMsgType.text:
-            for item in github_reviews.make_report(user, client, prs):
-                if 'progress' in item:
-                    ws.send_str(json.dumps(item))
-                else:
-                    data = {'item': item}
-                    response = aiohttp_jinja2.render_template('card.jinja2',
-                                                              request, data)
-                    ws.send_str(response.text)
-            ws.send_str('{"end": true}')
+    async with sse_response(request) as ws:
+        user = request.match_info['user']
+        (client, prs) = github_reviews.prepare_report(user)
+        for item in github_reviews.make_report(user, client, prs):
+            if 'progress' in item:
+                ws.send(json.dumps(item))
+            else:
+                data = {'item': item}
+                card = aiohttp_jinja2.render_template('card.jinja2',
+                                                      request, data)
+                ws.send(json.dumps({'card': card.text}))
+        ws.send(json.dumps({'end': 'true'}))
+        ws.force_close()
     return ws
 
-
-async def on_shutdown(app):
-    for ws in app['websockets']:
-        await ws.close(code=WSCloseCode.GOING_AWAY,
-                       message='Server shutdown')
 
 app = web.Application(debug=True)
 aiohttp_jinja2.setup(app, loader=jinja2.FileSystemLoader('templates'))
@@ -52,9 +44,6 @@ app.router.add_static('/static/', path='static', show_index=True)
 
 app.router.add_route('*', '/', root)
 app.router.add_route('GET', '/{user}', user_report)
-app.router.add_route('*', '/{user}/ws', ws, name='ws')
-
-app['websockets'] = []
-app.on_shutdown.append(on_shutdown)
+app.router.add_route('GET', '/{user}/ws', ws, name='ws')
 
 web.run_app(app)
